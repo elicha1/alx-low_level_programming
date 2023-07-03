0342e435c79f60a99c7a4f381de5513dd2a45adc
elicha1 <aicha95bouichou@gmail.com>
2023-07-03 14:14:11 -0700

    tertetet

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -10,6 +12,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-int _putchar(char);
 void print_diagsums(int *a, int size);
-#endif
+void set_string(char **s, char *to);
+#endif/*MAIN_H*/
