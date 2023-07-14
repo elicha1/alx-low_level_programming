462470e46d78313b01bbb3f5c10864b13213eb69
elicha1 <aicha95bouichou@gmail.com>
2023-07-14 06:43:42 -0700

    trtr

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,13 +1,12 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-int str_len(char *str);
-void *_calloc(unsigned int bytes, unsigned int size);
-void add_arrays(int *mul_result, int *sum_result, int len_r);
-int is_digit(char c);
-int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r);
-void print_me(int *sum_result, int len_r);
-int main(int argc, char **argv);
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -15,4 +14,5 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif
+
+#endif/*MAIN_H*/
