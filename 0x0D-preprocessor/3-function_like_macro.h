86c3d849f273afbecf0569df670cef53609884d4
elicha1 <aicha95bouichou@gmail.com>
2023-07-17 03:39:22 -0700

    fff

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,13 @@
-#ifndef ABS_H
-#define ABS_H
-#define ABS(x) (((x) < 0) ? ((x) * -1) : ((x) * ))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Auth: (cm-amos)Amos Mwongela
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
 #endif
