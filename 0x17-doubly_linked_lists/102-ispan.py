89261156ecb43e5558e2b439e489d8c961c37d3a
elicha1 <aicha95bouichou@gmail.com>
2023-11-09 12:48:20 -0800

    102

---
+++ 0x17-doubly_linked_lists/102-ispan.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+largest_palindrome = 0
+
+for i in range(100, 1000):
+    for j in range(i, 1000):  # To avoid duplicates and improve efficiency
+        product = i * j
+        if is_palindrome(product) and product > largest_palindrome:
+            largest_palindrome = product
+
+# Save the result to a file
+with open('102-result', 'w') as result_file:
+    result_file.write(str(largest_palindrome))
+
+print("Largest palindrome:", largest_palindrome)
